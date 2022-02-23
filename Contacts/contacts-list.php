1e603c605e6a29c704bc51d269274986215d8178
Romain Bohdanowicz <romain.bohdanowicz@gmail.com>
2022-02-23 17:16:15 +0100

    MVC

--- Contacts/contacts-list.php
+++ Contacts/contacts-list.php
@@ -1,1 +1,6 @@
 <?php
+require 'model.php';
+
+$contacts = get_all_contacts();
+
+require 'templates/contacts-list.php';
--- Contacts/model.php
+++ Contacts/model.php
@@ -1,1 +1,36 @@
 <?php
+function open_database_connection()
+{
+    /* Vous devriez toujours activer le rapport d'erreur pour mysqli avant une tentative de connexion */
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+    $mysqli = mysqli_connect('localhost', 'root', '', 'formation_php_2022_02');
+    /* Définir le jeu de caractère désiré après avoir établie une connexion */
+    mysqli_set_charset($mysqli, 'utf8mb4');
+
+    return $mysqli;
+}
+
+function close_database_connection(&$mysqli)
+{
+    mysqli_close($mysqli);
+    $mysqli = null;
+}
+
+function get_all_contacts()
+{
+    $mysqli = open_database_connection();
+
+    $sql = <<<SQL
+SELECT id, first_name, last_name, email, phone
+FROM contact
+LIMIT 0, 100
+SQL;
+
+    $result = mysqli_query($mysqli, $sql);
+
+    $contacts = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+    close_database_connection($mysqli);
+
+    return $contacts;
+}
